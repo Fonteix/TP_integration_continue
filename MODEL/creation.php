46a2614502373a992b379392ec50bef881dd75f2
Etienne Fonteix <etienne.fonteix@gmail.com>
2019-02-25 14:29:59 +0100

    Chamgement apparement ca marche pas

--- MODEL/creation.php
+++ MODEL/creation.php
@@ -1,7 +1,17 @@
 <?php
   include ('../bdd.php');
-  $reponse = $bdd->prepare('INSERT INTO clients (cli_nom, cli_adr, cli_cps, cli_ville, cli_email, cli_dnai, cli_pass)
-                            VALUES ($_POST['nom'], $_POST['adresse'], $_POST['codePostal'], $_POST['ville'], $_POST['email'], $_POST['dnai'], $_POST['password'])';
+
+  $nom=$_POST['nom'];
+  $adresse=$_POST['adresse'];
+  $codePostal=$_POST['codePostal'];
+  $ville=$_POST['ville'];
+  $email=$_POST['email'];
+  $dnai=$_POST['dnai'];
+  $password=$_POST['password'];
+
+  $reponse = $bdd->prepare("INSERT INTO clients (cli_nom, cli_adr, cli_cps, cli_ville, cli_email, cli_dnai, cli_pass)
+                            VALUES (?, ?, ?, ?, ?, ?, ?)");
+  $reponse->bind_param('sssssss', $nom, $adresse, $codePostal, $ville, $email, $dnai, $password);
   $reponse->execute();
-  $result = $reponse->get_result();
+  //$result = $reponse->get_result();
 ?>
