962c13cb3e51deeed572736c8aec00847db4fb0e
Etienne Fonteix <etienne.fonteix@gmail.com>
2019-02-25 14:03:04 +0100

    Changement inscription en cours

--- MODEL/creation.php
+++ MODEL/creation.php
@@ -1,6 +1,7 @@
 <?php
-  include 'bdd.php';
-  $reponse = $bdd->prepare('INSERT INTO clients (cli_nom, cli_adr, cli_cps, cli_ville, cli_email, cli_dnai, cli_pass) VALUES () ');
+  include ('../bdd.php');
+  $reponse = $bdd->prepare('INSERT INTO clients (cli_nom, cli_adr, cli_cps, cli_ville, cli_email, cli_dnai, cli_pass)
+                            VALUES ($_POST['nom'], $_POST['adresse'], $_POST['codePostal'], $_POST['ville'], $_POST['email'], $_POST['dnai'], $_POST['password'])';
   $reponse->execute();
   $result = $reponse->get_result();
 ?>
--- VIEW/inscription.php
+++ VIEW/inscription.php
@@ -1,9 +1,13 @@
-<?php
-  <form action="creation.php" method="post" enctype="multipart/form-data">
+
+  <form action="creation.php" method="post">
     <p>
-      Formulaire d'envoi de fichier :<br/>
-      <input type="file" name="monfichier" /><br/>
-      <input type="submit" value="Envoyer le fichier"/>
+      Nom <input type="text" name="nom"/> <br/>
+      Adresse <input type="text" name="adresse"/> <br/>
+      Code postal <input type="text" name="codePostal"/> <br/>
+      Ville <input type="text" name="ville"/> <br/>
+      Email <input type="text" name="email"/> <br/>
+      Date naissance <input type="text" name="dnai"/> <br/>
+      Mot de passe <input type="text" name="password"/> <br/>
+      <br/><input type="button" action="submit" value="S'inscrire" href="index.php"/>
     </p>
   </form>
-?>
